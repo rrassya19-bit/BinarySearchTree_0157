6a036ef753d0fa9516bac71650597c4536516f2a
rrassya19-bit <rrassys19@gmail.com>
2026-06-12 15:46:46 +0700

    feat: tambah fungsi main dan switch case

--- Binary_Search_Tree.cpp
+++ Binary_Search_Tree.cpp
@@ -67,7 +67,7 @@ public:
                 else
                     currentNode = currentNode->rightChild;
         }
-    }
+    } 
 
     void inorder(Node *ptr)
     {
@@ -116,4 +116,62 @@ public:
             cout << ptr->info << " ";
         }
     }
+
 };
+
+int main()
+{
+    BinaryTree obj;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. implement insert operation" << endl;
+        cout << "2. perform inorder traversal" << endl;
+        cout << "3. perform preorder traversal" << endl;
+        cout << "4. perform postorder traversal" << endl;
+        cout << "5. exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
+
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+        
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+        case '3':
+        {
+            obj.preorder(obj.ROOT);
+            break;
+        }
+        
+        case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
+        case '5':
+            return 0;
+        
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+    return 0;
+}
